7a6d5d0465738d5970df44e24956cd1034bd7536
Danny Le <le.dannyj@gmail.com>
2021-11-15 12:28:05 -0500

    Took out extra line in samples file and reworked code

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -11,22 +11,26 @@ module PolicyOcr
   ' _   |  |   ' => '7',
   ' _ |_||_|   ' => '8',
   ' _ |_| _|   ' => '9' }.freeze 
-	#Use .freeze on NUMBERS which will prevent the NUMBERS from being modifie.
+	#Use .freeze on NUMBERS which will prevent the NUMBERS from being modified.
 	
+	#Make global varible so it can be reached anywhere
+	@file_info = File.read("../spec/fixtures/sample.txt").split
+
 	#The 'self' points to the class or module that's in the process of being defined.
 	def self.policy_converter(policy_numbers)
 		#Open and read from a file one line at a time (for bigger files.)
-		file_info = File.read("policies").split
-		file_info.foreach("policy_numbers") { |line| puts line }
-		#Split the lines using the split method and define the number of lines by 4
-		split_line = file_info.split("\n").each_slice(4)
-		# Map each of the split lines and then map again to scan the single lines set by 3. Then make a grid by using the transponding method followed 
-		# by maping the grid into a number so it can be converted and matched with the pipe/underscored numbers. 
-		# After mapping you will want to join them together to form a hashed number in the format of NUMS. 
-		digits = split_line.map{ | new_lines | new_lines}.map{ | single_line | single_line.scan(/.../)}.transpose.map{ | number | NUMS[number.join]}.join
-		
+		File.foreach("../spec/fixtures/sample.txt") { |line| puts line }
+		# Each_slice the lines and define the number of lines by 4 then turn into an array where we will then map into the array twice to get our rows.
+		# Using the Scan method we can set 
+		split_line = @file_info.each_slice(4).to_a.map do | row_of_numbers |
+      row_of_numbers.map { | string | string.scan(/.../) }
+      # .transpose
+      # .map(&:join)
+		end
+	
 	end
 
 end
 
-
+#Displays the program on the console
+p PolicyOcr.policy_converter(@file_info)
