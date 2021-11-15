5a8ee9c49da88da55b91169fd0a2eef9fdfdd247
Danny Le <le.dannyj@gmail.com>
2021-11-14 20:30:24 -0500

    mapped the split lines

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -16,9 +16,12 @@ module PolicyOcr
 	#The 'self' points to the class or module that's in the process of being defined.
 	def self.policy_converter(policy_numbers)
 		#Open and read from a file one line at a time (for bigger files.)
-		file_data = File.read("policies").split
-		file_data.foreach("policy_numbers") { |line| puts line }
-		
+		file_info = File.read("policies").split
+		file_info.foreach("policy_numbers") { |line| puts line }
+		#Split the lines using the split method and define the number of lines by 4
+		split_line = file_info.split("\n").each_slice(4)
+		# Map each of the split lines
+		digits = split_line.map{ |new_lines| new_lines }
 	end
 
 end
