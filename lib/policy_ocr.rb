447ba1537fbcff3e3c8d4b1b479681408017b9f7
Danny Le <le.dannyj@gmail.com>
2021-11-15 14:16:01 -0500

    Got it to diplay numbers but there is a bug with 0

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -13,21 +13,26 @@ module PolicyOcr
   ' _ |_| _|   ' => '9' }.freeze 
 	#Use .freeze on NUMBERS which will prevent the NUMBERS from being modified.
 	
-	#Make global varible so it can be reached anywhere
-	@file_info = File.read("../spec/fixtures/sample.txt").split
+	#Make global varible so it can be reached anywhere and we will want to split the file at the new line mark
+	@file_info = File.read("../spec/fixtures/sample.txt").lines
 
 	#The 'self' points to the class or module that's in the process of being defined.
 	def self.policy_converter(policy_numbers)
 		#Open and read from a file one line at a time (for bigger files.)
 		File.foreach("../spec/fixtures/sample.txt") { |line| puts line }
-		# Each_slice the lines and define the number of lines by 4 then turn into an array where we will then map into the array twice to get our rows.
-		# Using the Scan method we can set 
-		split_line = @file_info.each_slice(4).to_a.map do | row_of_numbers |
-      row_of_numbers.map { | string | string.scan(/.../) }
-      # .transpose
-      # .map(&:join)
+		# Each_slice the lines and define the number of lines by 4 then turn it into an array where we will then map into the array twice to get our rows.
+		# Using the Scan method we can set to 3 collums
+		split_line = @file_info.each_slice(4).map do | row_of_numbers |
+      converted = row_of_numbers.map { | string | string.scan(/.{1,3}/) }
+			# Transpose this will convert any arguments to arrays, then merge elements of self with corresponding elements from each argument.
+			.transpose
+			# You will want it to match the hash format of your NUMS so they can be matched and converted later. We will use the map method again to achieve this.
+			# After mapping you will want to (&:join) them together to form a hashed number in the format of NUMBERS. 
+      .map(&:join)
+			#
+			.map { | number | NUMS[number]}
+			.join.to_i
 		end
-	
 	end
 
 end
