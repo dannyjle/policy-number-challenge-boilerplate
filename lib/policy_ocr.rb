0360ab019ec79b33433ac464429d638243a64041
Danny Le <le.dannyj@gmail.com>
2021-11-15 15:40:59 -0500

    Proof read code

--- lib/policy_ocr.rb
+++ lib/policy_ocr.rb
@@ -13,15 +13,14 @@ module PolicyOcr
   ' _ |_| _|   ' => '9' }.freeze 
 	#Use .freeze on NUMBERS which will prevent the NUMBERS from being modified.
 	
-	#Make global varible so it can be reached anywhere and we will want to split the file at the new line mark
+	#Make global varible so it can be reached anywhere and we will want to split the file at the new line mark.
 	@file_info = File.read("../spec/fixtures/sample.txt").lines
-
 	#The 'self' points to the class or module that's in the process of being defined.
 	def self.policy_converter(policy_numbers)
 		#Open and read from a file one line at a time (for bigger files.)
 		File.foreach("../spec/fixtures/sample.txt") { |line| puts line }
 		# Each_slice the lines and define the number of lines by 4 then turn it into an array where we will then map into the array twice to get our rows.
-		# Using the Scan method we can set to 3 collums
+		# Using the Scan method we can set it to 3 collums.
 		split_line = @file_info.each_slice(4).map do | row_of_numbers |
       converted = row_of_numbers.map { | string | string.scan(/.{1,3}/) }
 			# Transpose this will convert any arguments to arrays, then merge elements of self with corresponding elements from each argument.
