be55199a399bc7c79ebac23a36f38748d684cef9
Danny Le <le.dannyj@gmail.com>
2021-11-15 14:41:26 -0500

    Add PEDAC to README

--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -9,19 +9,4 @@ describe PolicyOcr do
     expect(fixture('sample').lines.count).to eq(44)
   end
 
-
-  # describe "policy_converter" do
-  #   context "given the 'sample'" do
-  #     it "converts policies" do
-  #       arr = [
-  #               000000000, 111111111, 222222222, 333333333,
-  #               444444444, 555555555, 666666666, 777777777,
-  #               888888888, 999999999, 123456789
-  #             ]
-  #       expect(PolicyOcr.policy_converter('../spec/fixtures/sample.txt')).to eq(array)
-  #     end
-  #   end
-  # end
-
-
 end
