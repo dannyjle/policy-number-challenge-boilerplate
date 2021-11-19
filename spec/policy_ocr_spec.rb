97ce4b95c5527401a593b9bea71f0ace52a1d0e4
Danny Le <le.dannyj@gmail.com>
2021-11-19 16:52:10 -0500

    Reworked code and created rspec test

--- spec/policy_ocr_spec.rb
+++ spec/policy_ocr_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/policy_ocr'
 
+# policy number: 3 4 5 8 8 2 8 6 5
+# position names: d9 d8 d7 d6 d5 d4 d3 d2 d1
+# checksum calculation:
+# (d1+(2*d2)+(3*d3)+...+(9*d9)) mod 11 = 0
+
 describe PolicyOcr do
   it "loads" do
     expect(PolicyOcr).to be_a Module
@@ -7,6 +12,10 @@ describe PolicyOcr do
 
   it 'loads the sample.txt' do
     expect(fixture('sample').lines.count).to eq(44)
+  end
+
+  it 'validates policy true' do
+    expect(PolicyOcr.checksum_valid('345882865')).to eq(true)
   end
 
 end
